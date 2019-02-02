aba1fa159c6ff19f78d556ed50261ed8e6efbe4b
Masahiro Nishiba <masahiro-nishiba@m3.com>
2019-02-02 22:16:17 +0900

    fix test

--- test/test_run.py
+++ test/test_run.py
@@ -31,9 +31,8 @@ class RunTest(unittest.TestCase):
     def test_run_with_undefined_environ(self):
         config_file_path = os.path.join(os.path.dirname(__name__), 'test_config.ini')
         luigi.configuration.LuigiConfigParser.add_config_path(config_file_path)
-        with self.assertRaises(SystemExit) as exit_code:
-            with self.assertRaises(luigi.parameter.MissingParameterException) as missing_parameter:
-                gokart.run()
+        with self.assertRaises(luigi.parameter.MissingParameterException) as missing_parameter:
+            gokart.run()
 
     @patch('sys.argv', new=['main', '--tree-info-mode=simple', '--tree-info-output-path=tree.txt', f'{__name__}._DummyTask', '--param', 'test', '--log-level=CRITICAL', '--local-scheduler'])
     @patch('luigi.LocalTarget', new=lambda path, **kwargs: luigi.mock.MockTarget(path, **kwargs))
