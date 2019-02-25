791a5ab37208144db218ea6bf116c2d8d1edd138
nishiba <nishiba.m@gmail.com>
2019-02-25 15:54:40 +0900

    Revert "read pipfile"

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup, find_packages
-from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
 
 readme_note = """\
 .. note::
@@ -12,18 +10,14 @@ readme_note = """\
 with open('README.md') as f:
     long_description = readme_note + f.read()
 
-pfile = Project(chdir=False).parsed_pipfile
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
-
 setup(
     name='gokart',
-    version='0.1.10',
+    version='0.1.9',
     description='A wrapper of luigi. This make it easy to define tasks.',
     long_description=long_description,
     author='M3, inc.',
     url='https://github.com/m3dev/gokart',
     license='MIT License',
-    install_requires=requirements,
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3.7',
