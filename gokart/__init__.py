b79815c84222e58a665b1054cbc279628a5f20b6
Masahiro Nishiba <masahiro-nishiba@m3.com>
2019-04-07 17:08:23 +0900

    add parater for list of task instance

--- gokart/__init__.py
+++ gokart/__init__.py
@@ -1,5 +1,5 @@
 from gokart.run import run
-from gokart.parameter import TaskInstanceParameter
+from gokart.parameter import TaskInstanceParameter, ListTaskInstanceParameter
 from gokart.task import TaskOnKart
 from gokart.info import make_tree_info, tree_info
 from gokart.workspace_management import delete_local_unnecessary_outputs
