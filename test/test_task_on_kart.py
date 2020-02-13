04d90f2786609837e4d37b576bbdee329c97f3c9
Masahiro Nishiba <masahiro-nishiba@m3.com>
2020-02-13 15:03:30 +0900

    fix unittest

--- test/test_task_on_kart.py
+++ test/test_task_on_kart.py
@@ -318,8 +318,8 @@ class TaskTest(unittest.TestCase):
 
         task = _Task(int_param=1, task_param=_SubTask(), list_task_param=[_SubTask(), _SubTask()])
         sub_task_id = _SubTask().make_unique_id()
-        expected = f'{__name__}._Task(int_param=1, task_param=test_task_on_kart._SubTask({sub_task_id}), ' \
-            f'list_task_param=[test_task_on_kart._SubTask({sub_task_id}), test_task_on_kart._SubTask({sub_task_id})])'
+        expected = f'{__name__}._Task(int_param=1, task_param={__name__}._SubTask({sub_task_id}), ' \
+            f'list_task_param=[{__name__}._SubTask({sub_task_id}), {__name__}._SubTask({sub_task_id})])'
         self.assertEqual(expected, str(task))
 
 
