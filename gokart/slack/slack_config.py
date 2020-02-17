7bc950a723a9caef6c067e746595d23e338f2002
田中　龍亮 <ryusuke.tanaka@m3-2019mac09.group-m3.com>
2020-02-17 17:27:44 +0900

    add send_tree_info parameter

--- gokart/slack/slack_config.py
+++ gokart/slack/slack_config.py
@@ -5,3 +5,6 @@ class SlackConfig(luigi.Config):
     token_name = luigi.Parameter(default='SLACK_TOKEN', description='slack token environment variable.')
     channel = luigi.Parameter(default='', description='channel name for notification.')
     to_user = luigi.Parameter(default='', description='Optional; user name who is supposed to be mentioned.')
+    send_tree_info = luigi.BoolParameter(default=True,
+                                         description='When this option is true, the dependency tree of tasks is included in send messeage.'
+                                                     'When notification takes long times, it is recommended to set false to this option.')
--- test/test_run.py
+++ test/test_run.py
@@ -1,11 +1,14 @@
 import os
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import luigi
 import luigi.mock
 
+
 import gokart
+from gokart.slack import SlackConfig
+from gokart.run import _try_to_send_event_summary_to_slack
 
 
 class _DummyTask(gokart.TaskOnKart):
@@ -45,6 +48,35 @@ class RunTest(unittest.TestCase):
         with self.assertRaises(SystemExit):
             gokart.run()
         self.assertTrue(gokart.make_tree_info(_DummyTask(param='test')), tree_info.output().load())
+
+    @patch('gokart.make_tree_info')
+    def test_try_to_send_event_summary_to_slack(self, make_tree_info_mock: MagicMock):
+        event_aggregator_mock = MagicMock()
+        event_aggregator_mock.get_summury.return_value = f'{__name__}._DummyTask'
+        event_aggregator_mock.get_event_list.return_value = f'{__name__}._DummyTask:[]'
+        make_tree_info_mock.return_value = 'tree'
+
+        def get_content(content: str, **kwargs):
+            self.output = content
+        slack_api_mock = MagicMock()
+        slack_api_mock.send_snippet.side_effect = get_content
+
+        cmdline_args = [f'{__name__}._DummyTask', '--param', 'test']
+        with patch('gokart.slack.SlackConfig.send_tree_info', True):
+            _try_to_send_event_summary_to_slack(slack_api_mock, event_aggregator_mock, cmdline_args)
+        expects = os.linesep.join(['===== Event List ====', event_aggregator_mock.get_event_list(), os.linesep])\
+            + os.linesep.join(['==== Tree Info ====', 'tree'])
+
+        results = self.output
+        self.assertEqual(expects, results)
+
+        cmdline_args = [f'{__name__}._DummyTask', '--param', 'test']
+        with patch('gokart.slack.SlackConfig.send_tree_info', False):
+            _try_to_send_event_summary_to_slack(slack_api_mock, event_aggregator_mock, cmdline_args)
+        expects = os.linesep.join(['===== Event List ====', event_aggregator_mock.get_event_list(), os.linesep])
+
+        results = self.output
+        self.assertEqual(expects, results)
 
 
 if __name__ == '__main__':
