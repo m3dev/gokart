c822c135053678718d93300dd86c6924d3000f9e
田中　龍亮 <ryusuke.tanaka@m3-2019mac09.group-m3.com>
2020-02-18 13:41:15 +0900

    `False` is set to default value

--- gokart/slack/slack_config.py
+++ gokart/slack/slack_config.py
@@ -5,6 +5,6 @@ class SlackConfig(luigi.Config):
     token_name = luigi.Parameter(default='SLACK_TOKEN', description='slack token environment variable.')
     channel = luigi.Parameter(default='', description='channel name for notification.')
     to_user = luigi.Parameter(default='', description='Optional; user name who is supposed to be mentioned.')
-    send_tree_info = luigi.BoolParameter(default=True,
+    send_tree_info = luigi.BoolParameter(default=False,
                                          description='When this option is true, the dependency tree of tasks is included in send messeage.'
                                                      'It is recommended to set false to this option when notification takes long time.')
