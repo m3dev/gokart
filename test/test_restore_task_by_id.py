ea047b7a2124b5e13066fee859984995655ce7bb
mski_iksm <9000000000000000091e@gmail.com>
2020-09-07 01:01:32 +0900

    fix test

--- test/test_restore_task_by_id.py
+++ test/test_restore_task_by_id.py
@@ -19,9 +19,6 @@ class _DummyTask(gokart.TaskOnKart):
     task_namespace = __name__
     sub_task = gokart.TaskInstanceParameter()
 
-    def requires(self):
-        return []
-
     def output(self):
         return self.make_target('test.txt')
 
