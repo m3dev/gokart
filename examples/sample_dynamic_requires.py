162221f66a6f5797c396183db7f224d574faf75a
mski_iksm <9000000000000000091e@gmail.com>
2021-02-13 02:41:06 +0900

    add isort skip not to conflict with yapf

--- examples/sample_dynamic_requires.py
+++ examples/sample_dynamic_requires.py
@@ -1,8 +1,7 @@
 import luigi
 
-import gokart
-
-'''
+import gokart  # noqa isort:skip
+"""
 Executes a Task using the results of a Task as dynamic parameters.
 
 For example, when we create a Task that generates model parameters.
@@ -14,11 +13,11 @@ We can using `gokart.TaskInstanceParameter` in gokart.
 Code: https://github.com/m3dev/gokart/blob/master/gokart/parameter.py
 
 luigi's dynamic-dependencies: https://luigi.readthedocs.io/en/stable/tasks.html#dynamic-dependencies
-'''
+"""
 
 
 class TaskA(gokart.TaskOnKart):
-    '''Generate parameters dynamically.'''
+    """Generate parameters dynamically."""
     sample = luigi.Parameter()
 
     def run(self):
