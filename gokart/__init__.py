b91c3493bd12177b4a50e800baccde24167e691c
6syun9 <6syun9@gmail.com>
2021-04-03 02:13:57 +0900

    fix fmt

--- gokart/__init__.py
+++ gokart/__init__.py
@@ -1,4 +1,4 @@
-from gokart.build import build, add_config
+from gokart.build import add_config, build
 from gokart.info import make_tree_info, tree_info
 from gokart.pandas_type_config import PandasTypeConfig
 from gokart.parameter import ExplicitBoolParameter, ListTaskInstanceParameter, TaskInstanceParameter
