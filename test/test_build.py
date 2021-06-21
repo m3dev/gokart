5abbf4bc55f4189503b095c6ac55a195559cd899
e-mon <emon18@icloud.com>
2021-06-21 11:46:31 +0900

    fix tests

--- test/test_build.py
+++ test/test_build.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from copy import copy
 
 import luigi
 import luigi.mock
@@ -21,7 +22,12 @@ class _DummyTask(gokart.TaskOnKart):
 class RunTest(unittest.TestCase):
     def setUp(self):
         luigi.configuration.LuigiConfigParser._instance = None
+        self.config_paths = copy(luigi.configuration.LuigiConfigParser._config_paths)
         luigi.mock.MockFileSystem().clear()
+        os.environ.clear()
+    
+    def tearDown(self) -> None:
+        luigi.configuration.LuigiConfigParser._config_paths = self.config_paths
         os.environ.clear()
 
     def test_build(self):
@@ -31,9 +37,9 @@ class RunTest(unittest.TestCase):
 
     def test_read_config(self):
         os.environ.setdefault('test_param', 'test')
-        config_file_path = os.path.join(os.path.dirname(__name__), 'config', 'test_config.ini')
+        config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', 'test_config.ini')
         gokart.utils.add_config(config_file_path)
-        output = gokart.build(_DummyTask())
+        output = gokart.build(_DummyTask(), reset_register=False)
         self.assertEqual(output, 'test')
 
 
