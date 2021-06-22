838c095195518f791d2ad9c8e51b2218696ffd26
e-mon <emon18@icloud.com>
2021-06-22 20:22:06 +0900

    Fix add_config behavior (#218)
    * append read_environ in add_config and implement some tests
    
    * fix tests
    
    * yapf

--- gokart/utils.py
+++ gokart/utils.py
@@ -24,3 +24,4 @@ def add_config(file_path: str):
     _, ext = os.path.splitext(file_path)
     luigi.configuration.core.parser = ext
     assert luigi.configuration.add_config_path(file_path)
+    read_environ()
