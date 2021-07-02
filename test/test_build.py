04d76b70320795c2f0d4ac2a0032626259ba7b34
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2021-07-01 18:47:50 -0700

    fixes #220 (#221)

--- test/test_build.py
+++ test/test_build.py
@@ -19,6 +19,19 @@ class _DummyTask(gokart.TaskOnKart):
         self.dump(self.param)
 
 
+class _DummyTaskTwoOutputs(gokart.TaskOnKart):
+    task_namespace = __name__
+    param1 = luigi.Parameter()
+    param2 = luigi.Parameter()
+
+    def output(self):
+        return {'out1': self.make_target('./test/dummy1.pkl'), 'out2': self.make_target('./test/dummy2.pkl')}
+
+    def run(self):
+        self.dump(self.param1, 'out1')
+        self.dump(self.param2, 'out2')
+
+
 class RunTest(unittest.TestCase):
     def setUp(self):
         luigi.configuration.LuigiConfigParser._instance = None
@@ -41,6 +54,14 @@ class RunTest(unittest.TestCase):
         gokart.utils.add_config(config_file_path)
         output = gokart.build(_DummyTask(), reset_register=False)
         self.assertEqual(output, 'test')
+
+    def test_build_dict_outputs(self):
+        param_dict = {
+            'out1': 'test1',
+            'out2': 'test2',
+        }
+        output = gokart.build(_DummyTaskTwoOutputs(param1=param_dict['out1'], param2=param_dict['out2']), reset_register=False)
+        self.assertEqual(output, param_dict)
 
 
 if __name__ == '__main__':
