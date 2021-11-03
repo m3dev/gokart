0f957e89dcfad9ccd3adb5290f1d0debd5fb0030
mski_iksm <9000000000000000091e@gmail.com>
2021-11-04 00:16:00 +0900

    fix " -> '

--- gokart/testing/pandas_assert.py
+++ gokart/testing/pandas_assert.py
@@ -13,16 +13,16 @@ def assert_frame_equal(actual: pd.DataFrame, expected: pd.DataFrame, **kwargs):
     - kwargs: Any
         Parameters passed to pandas.testing.assert_frame_equal.
     """
-    assert isinstance(actual, pd.DataFrame), "actual is not a DataFrame"
-    assert isinstance(expected, pd.DataFrame), "expected is not a DataFrame"
+    assert isinstance(actual, pd.DataFrame), 'actual is not a DataFrame'
+    assert isinstance(expected, pd.DataFrame), 'expected is not a DataFrame'
 
-    assert actual.index.is_unique, "actual index is not unique"
-    assert expected.index.is_unique, "expected index is not unique"
-    assert actual.columns.is_unique, "actual columns is not unique"
-    assert expected.columns.is_unique, "expected columns is not unique"
+    assert actual.index.is_unique, 'actual index is not unique'
+    assert expected.index.is_unique, 'expected index is not unique'
+    assert actual.columns.is_unique, 'actual columns is not unique'
+    assert expected.columns.is_unique, 'expected columns is not unique'
 
-    assert set(actual.columns) == set(expected.columns), "columns are not equal"
-    assert set(actual.index) == set(expected.index), "indexes are not equal"
+    assert set(actual.columns) == set(expected.columns), 'columns are not equal'
+    assert set(actual.index) == set(expected.index), 'indexes are not equal'
 
     expected_reindexed = expected.reindex(actual.index)[actual.columns]
     pd.testing.assert_frame_equal(actual, expected_reindexed, **kwargs)
