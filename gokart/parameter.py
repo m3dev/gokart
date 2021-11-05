d1cab1eddcd6f13c94c250c32d8799215ab0129c
vaaaaanquish <47341845+vaaaaanquish@users.noreply.github.com>
2021-11-06 08:24:25 +0900

    fix issue #254 (#257)
    * fix issue
    
    * yapf
    
    * isort
    
    * try/except
    
    * upper DictParameter
    
    * recursive decompress
    
    * staticmethod
    
    * fix ut

--- gokart/parameter.py
+++ gokart/parameter.py
@@ -1,26 +1,38 @@
+import bz2
 import json
+from logging import getLogger
 
 import luigi
 from luigi import task_register
+
+logger = getLogger(__name__)
 
 
 class TaskInstanceParameter(luigi.Parameter):
     @staticmethod
     def _recursive(param_dict):
-        params = dict(param_dict['params'])
+        params = param_dict['params']
         task_cls = task_register.Register.get_task_cls(param_dict['type'])
         for key, value in task_cls.get_params():
             if key in params:
                 params[key] = value.parse(params[key])
         return task_cls(**params)
 
+    @staticmethod
+    def _recursive_decompress(s):
+        s = dict(luigi.DictParameter().parse(s))
+        if 'params' in s:
+            s['params'] = TaskInstanceParameter._recursive_decompress(bz2.decompress(bytes.fromhex(s['params'])).decode())
+        return s
+
     def parse(self, s):
         if isinstance(s, str):
-            s = luigi.DictParameter().parse(s)
+            s = self._recursive_decompress(s)
         return self._recursive(s)
 
     def serialize(self, x):
-        values = dict(type=x.get_task_family(), params=x.to_str_params(only_significant=True))
+        params = bz2.compress(json.dumps(x.to_str_params(only_significant=True)).encode()).hex()
+        values = dict(type=x.get_task_family(), params=params)
         return luigi.DictParameter().serialize(values)
 
 
