589943526cc989e3f919084bff0f7d4326f124d3
mski_iksm <9000000000000000091e@gmail.com>
2023-11-09 23:36:40 +0900

    add

---
+++ gokart/task_complete_check.py
@@ -0,0 +1,9 @@
+from typing import Callable
+
+
+def task_complete_check_wrapper(run_func: Callable, complete_check_func: Callable):
+    def wrapper(*args, **kwargs):
+        if not complete_check_func():
+            run_func(*args, **kwargs)
+
+    return wrapper
