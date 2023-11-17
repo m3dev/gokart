b333b3610a0dd365547bd4c6d4b815dfda46fd71
Keisuke Ogaki <keisuke-ogaki@m3.com>
2023-11-17 20:03:00 +0900

    :white_check_mark: test PandasTypeCheck and build

--- test/test_pandas_type_check_framework.py
+++ test/test_pandas_type_check_framework.py
@@ -2,6 +2,7 @@ import unittest
 from logging import getLogger
 from typing import Any, Dict
 from unittest.mock import patch
+from gokart.build import GokartBuildError
 
 import luigi
 import pandas as pd
@@ -68,23 +69,22 @@ class TestPandasTypeCheckFramework(unittest.TestCase):
         luigi.setup_logging.DaemonLogging._configured = False
         luigi.setup_logging.InterfaceLogging._configured = False
 
-    @patch('sys.argv', new=['main', 'test_pandas_type_check_framework._DummyFailTask', '--log-level=CRITICAL', '--local-scheduler', '--no-lock'])
+
+    @patch('sys.argv', new=['main', 'test_pandas_type_check_framework._DummyFailTask', '--log-level=CRITICAL', '--local-scheduler',  '--no-lock'])
     @patch('luigi.LocalTarget', new=lambda path, **kwargs: MockTarget(path, **kwargs))
+    def test_fail_with_gokart_run(self):
+        with self.assertRaises(SystemExit) as exit_code:
+            gokart.run()
+        self.assertNotEqual(exit_code.exception.code, 0)  # raise Error
+
     def test_fail(self):
-        with self.assertRaises(SystemExit) as exit_code:
-            gokart.run()
-        self.assertNotEqual(exit_code.exception.code, 0)  # raise Error
+        with self.assertRaises(GokartBuildError):
+            gokart.build(_DummyFailTask())
 
-    @patch('sys.argv', new=['main', 'test_pandas_type_check_framework._DummyFailWithNoneTask', '--log-level=CRITICAL', '--local-scheduler', '--no-lock'])
-    @patch('luigi.LocalTarget', new=lambda path, **kwargs: MockTarget(path, **kwargs))
     def test_fail_with_None(self):
-        with self.assertRaises(SystemExit) as exit_code:
-            gokart.run()
-        self.assertNotEqual(exit_code.exception.code, 0)  # raise Error
+        with self.assertRaises(GokartBuildError):
+            gokart.build(_DummyFailWithNoneTask())
 
-    @patch('sys.argv', new=['main', 'test_pandas_type_check_framework._DummySuccessTask', '--log-level=CRITICAL', '--local-scheduler', '--no-lock'])
-    @patch('luigi.LocalTarget', new=lambda path, **kwargs: MockTarget(path, **kwargs))
     def test_success(self):
-        with self.assertRaises(SystemExit) as exit_code:
-            gokart.run()
-        self.assertEqual(exit_code.exception.code, 0)
+        gokart.build(_DummySuccessTask())
+        # no error
