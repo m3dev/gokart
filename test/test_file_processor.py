af8a8bab186eb260acb9fdf7b15c66f9924ba78f
yutaro-oguri <yutaro-oguri@m3.com>
2024-02-29 15:05:54 +0900

    add unittest for pickle processor

--- test/test_file_processor.py
+++ test/test_file_processor.py
@@ -4,66 +4,110 @@ import unittest
 import pandas as pd
 from luigi import LocalTarget
 
-from gokart.file_processor import CsvFileProcessor
+from gokart.file_processor import CsvFileProcessor, PickleFileProcessor
 
 
 class TestCsvFileProcessor(unittest.TestCase):
-
     def test_dump_csv_with_utf8(self):
-
-        df = pd.DataFrame({'あ': [1, 2, 3], 'い': [4, 5, 6]})
+        df = pd.DataFrame({"あ": [1, 2, 3], "い": [4, 5, 6]})
         processor = CsvFileProcessor()
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            temp_path = f'{temp_dir}/temp.csv'
+            temp_path = f"{temp_dir}/temp.csv"
 
             local_target = LocalTarget(path=temp_path, format=processor.format())
-            with local_target.open('w') as f:
+            with local_target.open("w") as f:
                 processor.dump(df, f)
 
             # read with utf-8 to check if the file is dumped with utf8
-            loaded_df = pd.read_csv(temp_path, encoding='utf-8')
+            loaded_df = pd.read_csv(temp_path, encoding="utf-8")
             pd.testing.assert_frame_equal(df, loaded_df)
 
     def test_dump_csv_with_cp932(self):
-        df = pd.DataFrame({'あ': [1, 2, 3], 'い': [4, 5, 6]})
-        processor = CsvFileProcessor(encoding='cp932')
+        df = pd.DataFrame({"あ": [1, 2, 3], "い": [4, 5, 6]})
+        processor = CsvFileProcessor(encoding="cp932")
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            temp_path = f'{temp_dir}/temp.csv'
+            temp_path = f"{temp_dir}/temp.csv"
 
             local_target = LocalTarget(path=temp_path, format=processor.format())
-            with local_target.open('w') as f:
+            with local_target.open("w") as f:
                 processor.dump(df, f)
 
             # read with cp932 to check if the file is dumped with cp932
-            loaded_df = pd.read_csv(temp_path, encoding='cp932')
+            loaded_df = pd.read_csv(temp_path, encoding="cp932")
             pd.testing.assert_frame_equal(df, loaded_df)
 
     def test_load_csv_with_utf8(self):
-        df = pd.DataFrame({'あ': [1, 2, 3], 'い': [4, 5, 6]})
+        df = pd.DataFrame({"あ": [1, 2, 3], "い": [4, 5, 6]})
         processor = CsvFileProcessor()
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            temp_path = f'{temp_dir}/temp.csv'
-            df.to_csv(temp_path, encoding='utf-8', index=False)
+            temp_path = f"{temp_dir}/temp.csv"
+            df.to_csv(temp_path, encoding="utf-8", index=False)
 
             local_target = LocalTarget(path=temp_path, format=processor.format())
-            with local_target.open('r') as f:
+            with local_target.open("r") as f:
                 # read with utf-8 to check if the file is dumped with utf8
                 loaded_df = processor.load(f)
                 pd.testing.assert_frame_equal(df, loaded_df)
 
     def test_load_csv_with_cp932(self):
-        df = pd.DataFrame({'あ': [1, 2, 3], 'い': [4, 5, 6]})
-        processor = CsvFileProcessor(encoding='cp932')
+        df = pd.DataFrame({"あ": [1, 2, 3], "い": [4, 5, 6]})
+        processor = CsvFileProcessor(encoding="cp932")
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            temp_path = f'{temp_dir}/temp.csv'
-            df.to_csv(temp_path, encoding='cp932', index=False)
+            temp_path = f"{temp_dir}/temp.csv"
+            df.to_csv(temp_path, encoding="cp932", index=False)
 
             local_target = LocalTarget(path=temp_path, format=processor.format())
-            with local_target.open('r') as f:
+            with local_target.open("r") as f:
                 # read with cp932 to check if the file is dumped with cp932
                 loaded_df = processor.load(f)
                 pd.testing.assert_frame_equal(df, loaded_df)
+
+
+class TestPickleFileProcessor(unittest.TestCase):
+    def test_dump_and_load_normal_obj(self):
+        var = "abc"
+        processor = PickleFileProcessor()
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_path = f"{temp_dir}/temp.pkl"
+            local_target = LocalTarget(path=temp_path, format=processor.format())
+            with local_target.open("w") as f:
+                processor.dump(var, f)
+            with local_target.open("r") as f:
+                loaded = processor.load(f)
+
+        self.assertEqual(loaded, var)
+
+    def test_dump_and_load_class(self):
+        import functools
+
+        def plus1(func):
+            @functools.wraps(func)
+            def wrapped() -> int:
+                ret = func()
+                return ret + 1
+
+            return wrapped
+
+        class A:
+            def __init__(self) -> None:
+                self.run = plus1(self.run)
+
+            def run(self) -> int:
+                return 1
+
+        obj = A()
+        processor = PickleFileProcessor()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_path = f"{temp_dir}/temp.pkl"
+            local_target = LocalTarget(path=temp_path, format=processor.format())
+            with local_target.open("w") as f:
+                processor.dump(obj, f)
+            with local_target.open("r") as f:
+                loaded = processor.load(f)
+
+        self.assertEqual(loaded.run(), obj.run())
