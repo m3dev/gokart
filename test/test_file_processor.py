7a00e34ce58ecc618447e8a0e5a87fed655539dd
Yutaro Oguri <63549742+maronuu@users.noreply.github.com>
2024-04-23 15:49:41 +0900

    Use `dill` instead of `pickle` for processing `.pkl` files (#354)
    * use dill instead of pickle for file processor
    
    * add unittest for pickle processor
    
    * relock
    
    * fix unittest
    
    * fmt
    
    * rename dill
    
    * relock
    
    * relock with poetry 1.8.2
    
    * relock
    
    * lock
    
    * fmt
    
    * relock
    
    ---------
    
    Co-authored-by: yutaro-oguri <yutaro-oguri@m3.com>

--- test/test_file_processor.py
+++ test/test_file_processor.py
@@ -1,10 +1,11 @@
 import tempfile
 import unittest
+from typing import Callable
 
 import pandas as pd
 from luigi import LocalTarget
 
-from gokart.file_processor import CsvFileProcessor, FeatherFileProcessor
+from gokart.file_processor import CsvFileProcessor, FeatherFileProcessor, PickleFileProcessor
 
 
 class TestCsvFileProcessor(unittest.TestCase):
@@ -65,6 +66,54 @@ class TestCsvFileProcessor(unittest.TestCase):
                 # read with cp932 to check if the file is dumped with cp932
                 loaded_df = processor.load(f)
                 pd.testing.assert_frame_equal(df, loaded_df)
+
+
+class TestPickleFileProcessor(unittest.TestCase):
+    def test_dump_and_load_normal_obj(self):
+        var = 'abc'
+        processor = PickleFileProcessor()
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_path = f'{temp_dir}/temp.pkl'
+            local_target = LocalTarget(path=temp_path, format=processor.format())
+            with local_target.open('w') as f:
+                processor.dump(var, f)
+            with local_target.open('r') as f:
+                loaded = processor.load(f)
+
+        self.assertEqual(loaded, var)
+
+    def test_dump_and_load_class(self):
+        import functools
+
+        def plus1(func: Callable[[], int]) -> Callable[[], int]:
+            @functools.wraps(func)
+            def wrapped() -> int:
+                ret = func()
+                return ret + 1
+
+            return wrapped
+
+        class A:
+            run: Callable[[], int]
+
+            def __init__(self) -> None:
+                self.run = plus1(self.run)
+
+            def run(self) -> int:
+                return 1
+
+        obj = A()
+        processor = PickleFileProcessor()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_path = f'{temp_dir}/temp.pkl'
+            local_target = LocalTarget(path=temp_path, format=processor.format())
+            with local_target.open('w') as f:
+                processor.dump(obj, f)
+            with local_target.open('r') as f:
+                loaded = processor.load(f)
+
+        self.assertEqual(loaded.run(), obj.run())
 
 
 class TestFeatherFileProcessor(unittest.TestCase):
