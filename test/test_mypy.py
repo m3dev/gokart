59460f1a4c4133b4aa48e6d53a1df5d6a6370090
Hironori Yamamoto <mr.nikoru918@gmail.com>
2024-07-30 00:35:06 +0900

    chore: add tests for gokart parameters

--- test/test_mypy.py
+++ test/test_mypy.py
@@ -17,8 +17,9 @@ class MyTask(gokart.TaskOnKart):
     # NOTE: mypy shows attr-defined error for the following lines, so we need to ignore it.
     foo: int = luigi.IntParameter() # type: ignore
     bar: str = luigi.Parameter() # type: ignore
+    baz: bool = gokart.ExplicitBoolParameter()
 
-MyTask(foo=1, bar='bar')
+MyTask(foo=1, bar='bar', baz=False)
 """
 
         with tempfile.NamedTemporaryFile(suffix='.py') as test_file:
@@ -37,10 +38,11 @@ class MyTask(gokart.TaskOnKart):
     # NOTE: mypy shows attr-defined error for the following lines, so we need to ignore it.
     foo: int = luigi.IntParameter() # type: ignore
     bar: str = luigi.Parameter() # type: ignore
+    baz: bool = gokart.ExplicitBoolParameter()
 
 # issue: foo is int
 # not issue: bar is missing, because it can be set by config file.
-MyTask(foo='1')
+MyTask(foo='1', baz='not bool')
         """
 
         with tempfile.NamedTemporaryFile(suffix='.py') as test_file:
@@ -48,4 +50,5 @@ MyTask(foo='1')
             test_file.flush()
             result = api.run(['--no-incremental', '--cache-dir=/dev/null', '--config-file', str(PYPROJECT_TOML), test_file.name])
             self.assertIn('error: Argument "foo" to "MyTask" has incompatible type "str"; expected "int"  [arg-type]', result[0])
-            self.assertIn('Found 1 error in 1 file (checked 1 source file)', result[0])
+            self.assertIn('error: Argument "baz" to "MyTask" has incompatible type "str"; expected "bool"  [arg-type]', result[0])
+            self.assertIn('Found 2 errors in 1 file (checked 1 source file)', result[0])
