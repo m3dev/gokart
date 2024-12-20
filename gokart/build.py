656de9d34d9173f54a0f2e3112d78cf51c3859c7
mski_iksm <9000000000000000091e@gmail.com>
2024-12-20 14:33:18 +0900

    use gokart.WorkerSchedulerFactory

--- gokart/build.py
+++ gokart/build.py
@@ -148,7 +148,8 @@ def build(
         def _build_task():
             task_lock_exception_raised.flag = False
             result = luigi.build(
-                [task],
+                tasks=[task],
+                worker_scheduler_factory=WorkerSchedulerFactory(),
                 local_scheduler=True,
                 detailed_summary=True,
                 log_level=logging.getLevelName(log_level),
