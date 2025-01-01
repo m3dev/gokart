e5f826cf6a40860d609ea05c0670f317381dc9ad
Keisuke OGAKI <hikingko1@gmail.com>
2025-01-02 01:10:02 +0900

    Update gokart/task.py
    Co-authored-by: hirosassa <hiro.sassa@gmail.com>

--- gokart/task.py
+++ gokart/task.py
@@ -377,7 +377,7 @@ If you want to specify `required_columns` and `drop_columns`, please extract the
     def to_str_params(self, only_significant=False, only_public=False):
         if only_significant and (not only_public):
             # cache to_str_params to avoid too slow task creation of deep task tree
-            # e.g. gokart.build(RecursiveTask(dep=RecursiveTask(dep=RecursiveTask(dep=HelloWorldTask())))) takes O(n*n) to_str_params calls
+            # e.g. gokart.build(RecursiveTask(dep=RecursiveTask(dep=RecursiveTask(dep=HelloWorldTask())))) needs O(n^2) times to_str_params calls with respect to n times RecursiveTask
             if self._str_params_cache is not None:
                 return self._str_params_cache
             else:
