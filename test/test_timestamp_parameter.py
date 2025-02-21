729f7d8b867376b5540adb840157fae824e12d0e
Shuntaro Takahashi <shuntaro-takahashi@m3.com>
2025-02-21 18:59:13 +0900

    add tests

--- test/test_timestamp_parameter.py
+++ test/test_timestamp_parameter.py
@@ -24,18 +24,38 @@ class TimestampParameterTaskWithDefault(TaskOnKart):
 
 
 class TimestampParameterTest(unittest.TestCase):
+    def setUp(self):
+        self.default_datetime = datetime.datetime(2025, 2, 21, 12, 0, 0, tzinfo=datetime.timezone(datetime.timedelta(hours=9)))
+        self.default_datetime_str = '2025-02-21T120000Z+0900'
+
     def test_default(self):
         with CmdlineParser.global_instance([f'{__name__}.TimestampParameterTaskWithDefault']) as cp:
-            assert cp.get_task_obj().timestamp == datetime.datetime(2025, 2, 21, 12, 0, 0, tzinfo=datetime.timezone(datetime.timedelta(hours=9)))
+            assert cp.get_task_obj().timestamp == self.default_datetime
 
     def test_parse_param(self):
-        with CmdlineParser.global_instance([f'{__name__}.TimestampParameterTaskWithDefault', '--timestamp', '2025-02-21T120000Z+0900']) as cp:
-            assert cp.get_task_obj().timestamp == datetime.datetime(2025, 2, 21, 12, 0, 0, tzinfo=datetime.timezone(datetime.timedelta(hours=9)))
+        with CmdlineParser.global_instance([f'{__name__}.TimestampParameterTaskWithDefault', '--timestamp', '2024-01-20T110000Z+0000']) as cp:
+            assert cp.get_task_obj().timestamp == datetime.datetime(2024, 1, 20, 11, 0, 0, tzinfo=datetime.timezone(datetime.timedelta(hours=0)))
 
     def test_value_error_without_timezone_input(self):
         with pytest.raises(ValueError):
             with CmdlineParser.global_instance([f'{__name__}.TimestampParameterTaskWithoutDefault', '--timestamp', '2025-02-21T120000']) as cp:
                 cp.get_task_obj()
+
+    def test_parse_method(self):
+        actual = TimestampParameter().parse(self.default_datetime_str)
+        expected = self.default_datetime
+        self.assertEqual(actual, expected)
+
+    def test_serialize_method(self):
+        actual = TimestampParameter().serialize(self.default_datetime)
+        expected = self.default_datetime_str
+        self.assertEqual(actual, expected)
+
+    def test_serialize_task(self):
+        task = TimestampParameterTaskWithoutDefault(timestamp=self.default_datetime)
+        actual = str(task)
+        expected = f'(timestamp={self.default_datetime_str})'
+        self.assertTrue(actual.endswith(expected))
 
 
 if __name__ == '__main__':
