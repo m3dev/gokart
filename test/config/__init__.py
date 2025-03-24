f8432e79ebbbe11f93a7c9f7ae690f32100161f8
Hironori Yamamoto <hironori-yamamoto@m3.com>
2025-03-24 16:56:22 +0900

    feat: add gokart mypy options to handle missing parameters

--- test/config/__init__.py
+++ test/config/__init__.py
@@ -3,4 +3,5 @@ from typing import Final
 
 CONFIG_DIR: Final[Path] = Path(__file__).parent.resolve()
 PYPROJECT_TOML: Final[Path] = CONFIG_DIR / 'pyproject.toml'
+PYPROJECT_TOML_SET_DISALLOW_MISSING_PARAMETERS: Final[Path] = CONFIG_DIR / 'pyproject_disallow_missing_parameters.toml'
 TEST_CONFIG_INI: Final[Path] = CONFIG_DIR / 'test_config.ini'
